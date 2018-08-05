a7098dba5715b0ded19541ab8e00a92b46138c2c
vesche <vesche@protonmail.com>
2018-08-04 20:13:19 -0400

    Update unittests

--- ipshaman/tests/ipshaman_tests.py
+++ ipshaman/tests/ipshaman_tests.py
@@ -3,41 +3,22 @@
 import unittest
 
 from ipshaman.core import client
-from ipshaman.cli.filt import parse_filter, parse_data
 
 c = client.Client()
 
 
 class IpshamanTests(unittest.TestCase):
 
-    def test_parse_filter(self):
-        self.assertEqual(
-            parse_filter("country_code=US,city=Boulder"),
-            {"country_code":"US", "city":"Boulder"}
-        )
-
-    def test_parse_data(self):
-        self.assertEqual(
-            parse_data({'a':'b','c':'d'}, {'a':'b'}),
-            {'a':'b','c':'d'}
-        )
-    
     def test_geoip_invalid(self):
         self.assertEqual(
-            c.geoip('0.0.0.0')['error'],
+            c.lookup('0.0.0.0')['error'],
             'INVALID_IP'
         )
     
     def test_geoip_valid(self):
         self.assertEqual(
-            c.geoip('8.8.8.8')['latitude'],
+            c.lookup('8.8.8.8')['latitude'],
             37.7509994507
-        )
-
-    def test_rdap_valid(self):
-        self.assertEqual(
-            c.rdap('8.8.8.8')['asn_description'],
-            'GOOGLE - Google LLC, US'
         )
 
 
